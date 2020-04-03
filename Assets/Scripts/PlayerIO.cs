e2d089cf8781dcd0b7d7134559e22ab287e4f2ce
Lucas Trognon <lucas.trognon@gmail.com>
2020-04-03 17:02:41 +0200

    Added Player which can jump and move and 2 new scenes

---
+++ Assets/Scripts/PlayerIO.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMover))]
+public class PlayerIO : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private float jumpForce = 40f;
+    [SerializeField]
+    private float lookSensitivity = 5f;
+    [SerializeField]
+    // Start is called before the first frame update
+
+    private PlayerMover mover;
+    
+    void Start()
+    {
+        mover = GetComponent<PlayerMover>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+        Cursor.lockState = CursorLockMode.Locked;
+        Vector3 _movHorizontal = transform.right*_xMov;
+        Vector3 _movVertical = transform.forward*_zMov;
+        Vector3 _velocity = (_movHorizontal+_movVertical).normalized * speed;
+        mover.Move(_velocity);
+        float _jump = Input.GetAxisRaw("Jump");
+        mover.Jump(_jump*transform.up*jumpForce);
+
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yRotation*lookSensitivity, 0);
+        mover.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0);
+        mover.RotateCamera(_cameraRotation*lookSensitivity);
+
+  
+
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private bool grounded = false;
+    private Vector3 cameraRotation = Vector3.zero;
+    private Vector3 force = Vector3.zero;
+    private Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    public void Move(Vector3 _velocity){
+        velocity = _velocity;
+    }
+    public void Rotate(Vector3 _rotation){
+        rotation = _rotation;
+    }
+    public void Jump(Vector3 _jump){
+        if (grounded){
+        force+=_jump;
+        }
+    }
+     public void ApplyForce(Vector3 _force){
+        
+    force+=_force;
+        
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation){
+        cameraRotation = _cameraRotation;
+    }
+
+
+    void FixedUpdate(){
+        ApplyForce();
+        PerformMovement();
+        PerformRotation();
+        PerformCameraRotation();
+       
+        
+    }
+    void PerformMovement(){
+        if (velocity!=Vector3.zero){
+            rigidBody.MovePosition(rigidBody.position+velocity*Time.fixedDeltaTime);
+        }
+    }
+    void PerformRotation(){
+        if (rotation!=Vector3.zero){
+            rigidBody.MoveRotation(rigidBody.rotation*Quaternion.Euler(rotation));
+        }
+    }
+    void PerformCameraRotation(){
+        if (cam!=null){
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+
+
+    void ApplyForce(){
+        if (force!=Vector3.zero){
+            rigidBody.AddForce(force);
+            force = Vector3.zero;
+        }
+    }
+    void OnCollisionEnter(Collision collisionInfo){
+        if(collisionInfo.collider.tag == "Ground"){
+          grounded = true;
+      }
+    }
+    void OnCollisionExit(Collision collisionInfo){
+         if(collisionInfo.collider.tag == "Ground"){
+          grounded = false;
+      }
+    }
+
+
+}
