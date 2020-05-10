02a51df8445fc41d9be2fc4b425c8e2460f6db34
Camille Montemagni <camille.montemagni@epfl.ch>
2020-05-10 17:26:54 +0200

    Recreated entire networking framework not in VR for now

---
+++ Assets/Scripts/TestPlayerAnimatorManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Photon.Realtime;
+using Photon.Pun;
+
+
+namespace BachelorProject
+{
+    public class TestPlayerAnimatorManager : MonoBehaviourPun
+    {
+        #region Private Fields
+
+        private Animator animator;
+
+        [SerializeField]
+        private float directionDampTime = 0.25f;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (!animator)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+            {
+                return;
+            }
+            if (!animator)
+            {
+                return;
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0)
+            {
+                v = 0;
+            }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/TestPlayerManager.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Photon.Pun;
+
+using System.Collections;
+
+namespace BachelorProject
+{
+    /// <summary>
+    /// Player manager.
+    /// Handles fire Input and Beams.
+    /// </summary>
+    public class TestPlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+        #region Public Fields
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        public void Start()
+        {
+            TestCameraWork _cameraWork = gameObject.GetComponent<TestCameraWork>();
+
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><b>Missing</b></Color> CameraWork Component on player Prefab.", this);
+            }
+
+            #if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+			UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
+        }
+
+        public override void OnDisable()
+		{
+			// Always call the base to remove callbacks
+			base.OnDisable ();
+
+			#if UNITY_5_4_OR_NEWER
+			UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+			#endif
+		}
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity on every frame.
+        /// </summary>
+        void Update()
+        {
+        }
+
+        #if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+        #endif
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        #if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+        {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+        #endif
+
+        #endregion
+
+
+        #region IPunObservable implementation
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+        }
+
+        #endregion
+    }
+}
