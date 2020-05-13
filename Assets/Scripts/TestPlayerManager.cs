28d10edccddc1e1805d5910e1c5b6e16892cbe35
Camille Montemagni <camille.montemagni@epfl.ch>
2020-05-13 15:06:03 +0200

    Networking test : master movements won't show in client, trying to debug, removed DontDestroyOnLoad statement in TestPlayerManager

--- Assets/Scripts/TestPlayerManager.cs
+++ Assets/Scripts/TestPlayerManager.cs
@@ -35,8 +35,8 @@ namespace BachelorProject
             }
             // #Critical
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
-            DontDestroyOnLoad(this.gameObject);
-            DontDestroyOnLoad(LocalPlayerInstance);
+            //DontDestroyOnLoad(this.gameObject);
+            //DontDestroyOnLoad(LocalPlayerInstance);
         }
 
         /// <summary>
