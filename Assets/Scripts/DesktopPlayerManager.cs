1140c0ba363b8aa3e2a04403cb1d14ac89907f79
Camille Montemagni <camille.montemagni@epfl.ch>
2020-05-28 21:06:21 +0200

    reset server settings and comment DontDestroyOnLoad in DesktopPlayerManager

--- Assets/Scripts/DesktopPlayerManager.cs
+++ Assets/Scripts/DesktopPlayerManager.cs
@@ -37,7 +37,7 @@ namespace DesktopProject
             }
             // #Critical
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
-            DontDestroyOnLoad(this.gameObject);
+            //DontDestroyOnLoad(this.gameObject);
         }
 
         /// <summary>
