78ccfbc8bf216554b4a9af05aba7bb6077596036
Camille Montemagni <camille.montemagni@epfl.ch>
2020-06-03 15:16:46 +0200

    VOICE WTF ?

--- Assets/Scripts/DesktopGameManager.cs
+++ Assets/Scripts/DesktopGameManager.cs
@@ -32,7 +32,7 @@ namespace DesktopProject
                 {
                     Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                     // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
                 }
                 else
                 {
